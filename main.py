9a032b97650cb798d7b9b9028c5d5e1fce5257a9
Wilton Ribeiro <wiltonribeiromaster@gmail.com>
2020-11-29 23:52:41 -0300

    conectando ao Discord e enviando ao terminal o horário da conexão

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 # importando módulos 
 import os
-# importando o módulo random
-import random
 from dotenv import load_dotenv
 from discord.ext import commands
+from datetime import datetime
 
 # pegando o token e o prefixo do bot
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 PREFIX = os.getenv('PREFIX')
 
-print(TOKEN)
-print(PREFIX)
+# definindo o prefixo do bot e armazenando o objeto 'bot'
+bot = commands.Bot(command_prefix='!')
+
+# mostrando algo no terminal quando o client se conectar
+@bot.event
+async def on_ready():
+    nome_bot = bot.user
+    hora = datetime.now().strftime('%d/%m/%Y %H:%M') 
+    print('{0} se conectou com sucesso :) em: {1}'.format(nome_bot, hora))
+
+bot.run(TOKEN)
