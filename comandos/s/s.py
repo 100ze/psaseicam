be82edb93d8c177624037a32030b377e815d4d75
Wilton Ribeiro <wiltonribeiromaster@gmail.com>
2020-11-30 14:06:30 -0300

    coletando informações básicas do bot, como nome e icon url

--- comandos/s/s.py
+++ comandos/s/s.py
@@ -2,7 +2,17 @@
 # importando módulos 
 import random
 
+async def criaEmbeds(ctx):
+    bot_info = await ctx.bot.application_info() 
+    imagem_bot = bot_info.icon_url
+    nome_bot = bot_info.name 
+
+    print(nome_bot)
+    print(imagem_bot)
+
 async def comando_s(ctx, pergunta):
+    await criaEmbeds(ctx)
+
     # recebendo um número aleatório entre de 0 a 1
     resposta = random.randint(0,1)
 
